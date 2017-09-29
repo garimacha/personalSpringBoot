d23ebf90cdc0393f99793fd363705635abc6d1e3
Garima Chandra <garich01@noa.nintendo.com>
2017-09-29 13:10:47 -0700

    added the test for the endpoint

--- src/test/java/com/example/demo/PersonalSpringBootApplicationTests.java
+++ src/test/java/com/example/demo/PersonalSpringBootApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example.demo;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static com.jayway.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PersonalSpringBootApplicationTests {
 
+    private static final String PING_END_POINT = "/ping";
+
+    @Value("${local.server.port}")
+    private int serverPort;
+
+    @Before
+    public void setup() {
+        RestAssured.port = serverPort;
+    }
+
 	@Test
-	public void contextLoads() {
-	}
+    public void validateGetPing() {
+        Response response = given().log().all().when().then().get(PING_END_POINT);
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        assertThat(response.body().asString()).isEqualTo("Hello World");
+    }
 
 }
